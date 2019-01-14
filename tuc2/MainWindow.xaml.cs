4fb9b2a75308e8b20c4a81ae148c416dc352504a
Jeidoz <havruk0stas@gmail.com>
2019-01-14 19:54:44 +0200

    Cleaning up before merge

--- tuc2/MainWindow.xaml.cs
+++ tuc2/MainWindow.xaml.cs
@@ -28,62 +28,6 @@ namespace tuc2
             InitializeComponent();
 
             dbContext = new ApplicationContext();
-            dbContext.Users.Load();
-            this.DataContext = dbContext.Users.Local.ToBindingList();
-        }
-
-        // добавление
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            UserWindow UserWindow = new UserWindow(new User());
-            if (UserWindow.ShowDialog() == true)
-            {
-                User User = UserWindow.User;
-                dbContext.Users.Add(User);
-                dbContext.SaveChanges();
-            }
-        }
-        // редактирование
-        private void Edit_Click(object sender, RoutedEventArgs e)
-        {
-            // если ни одного объекта не выделено, выходим
-            if (UsersList.SelectedItem == null) return;
-            // получаем выделенный объект
-            User User = UsersList.SelectedItem as User;
-
-            UserWindow UserWindow = new UserWindow(new User
-            {
-                Id = User.Id,
-                Login = User.Login,
-                Password = User.Password,
-                FirstName = User.FirstName,
-                LastName = User.LastName
-            });
-
-            if (UserWindow.ShowDialog() == true)
-            {
-                // получаем измененный объект
-                User = dbContext.Users.Find(UserWindow.User.Id);
-                if (User != null)
-                {
-                    User.Login = UserWindow.User.Login;
-                    User.FirstName = UserWindow.User.FirstName;
-                    User.LastName = UserWindow.User.LastName;
-                    User.Password = UserWindow.User.Password;
-                    dbContext.Entry(User).State = EntityState.Modified;
-                    dbContext.SaveChanges();
-                }
-            }
-        }
-        // удаление
-        private void Delete_Click(object sender, RoutedEventArgs e)
-        {
-            // если ни одного объекта не выделено, выходим
-            if (UsersList.SelectedItem == null) return;
-            // получаем выделенный объект
-            User User = UsersList.SelectedItem as User;
-            dbContext.Users.Remove(User);
-            dbContext.SaveChanges();
         }
     }
 }
