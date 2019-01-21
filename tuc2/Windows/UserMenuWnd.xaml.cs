e8ff31982e5232ee00bbb90977b0b9312e0342d3
Jeidoz <havruk0stas@gmail.com>
2019-01-21 11:07:28 +0200

    Added ability to open task solver by user

--- tuc2/Windows/UserMenuWnd.xaml.cs
+++ tuc2/Windows/UserMenuWnd.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tuc2.Entities;
+using tuc2.Windows.UserControls;
 
 namespace tuc2.Windows
 {
@@ -20,9 +22,45 @@ namespace tuc2.Windows
     /// </summary>
     public partial class UserMenuWnd : UserControl
     {
-        public UserMenuWnd()
+        public User LoginedUser { get; set; }
+
+        public UserMenuWnd(User user)
         {
+            LoginedUser = user;
             InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UserControl usc = null;
+            GridMain.Children.Clear();
+            ListViewItem item = ((ListView)sender).SelectedItem as ListViewItem;
+            MainWindow mainWindow = null;
+            switch (item.Name)
+            {
+                case "ItemHome":
+                    usc = new TaskSolverWnd();
+                    GridMain.Children.Add(usc);
+                    break;
+                case "ItemLogout":
+                    mainWindow = (MainWindow)Window.GetWindow(this);
+                    mainWindow.ShowLoginWindow();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
