5fbffbcf3c3d64ba950e0ad4489d2c3f19be4c2c
Jeidoz <havruk0stas@gmail.com>
2019-02-28 21:11:04 +0200

    Created ViewModels for MVVM pattern

---
+++ tuc2/ViewModels/Media.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuc2.ViewModels
+{
+    public class Media
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ tuc2/ViewModels/UserViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuc2.ViewModels
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private string login;
+        private string password;
+        private string firstName;
+        private string lastName;
+
+        public int Id { get; set; }
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string RoleType { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
